7bcd75358b83b6a3faa17e3f934580b8a2f6214d
Agustin Alberdi Hernandez <agustin.hernandez@whitehatgaming.com>
2023-06-13 19:01:53 +0200

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,20 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import EventListView from '../views/EventListView.vue';
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'event-list',
+      component: EventListView
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: () => import('../views/AboutView.vue')
+    }
+  ]
+})
+
+export default router
---
+++ src/views/EventListView.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { ref } from 'vue'
+import axios from 'axios';
+import EventCard from '../components/EventCard.vue'
+
+const events = ref(null)
+</script>
+
+<template>
+  <h1>Events For Good</h1>
+  <div class="events">
+    <EventCard v-for="event in events" :key="event.id" :event="event"></EventCard>
+  </div>
+</template>
+
+<style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
